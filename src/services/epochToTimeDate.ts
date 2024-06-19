4f8e38ccdb9a1cbef288938f7ddbe546e7de5dc2
Victor Ly <ly.victor2000@gmail.com>
2024-06-19 13:20:42 +0200

    use month names instead of number

--- src/services/epochToTimeDate.ts
+++ src/services/epochToTimeDate.ts
@@ -7,7 +7,22 @@ const epochToTimeDate = (epoch: number): string => {
   const hours = ("0" + date.getHours()).slice(-2);
   const minutes = ("0" + date.getMinutes()).slice(-2);
 
-  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
+  const mapMonth: { [key: string]: string } = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
+  };
+
+  const formattedDate = `${day} ${mapMonth[month]} ${year} - ${hours}:${minutes}`;
 
   return formattedDate;
 }
