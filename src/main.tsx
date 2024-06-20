8dc8658130a7fc009b2b277e8a46252b4b7f2284
Victor Ly <ly.victor2000@gmail.com>
2024-06-20 14:44:35 +0200

    remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import { ToastContainer, Slide, ToastContainerProps } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./styles.scss";
 import router from "./router.tsx";
 
@@ -27,7 +26,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ToastContainer {...toastOptions} />
-      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   </React.StrictMode>
 )
